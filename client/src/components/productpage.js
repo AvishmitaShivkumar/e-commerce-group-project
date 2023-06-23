a8002e8b177acaf90ec4f679bbea2cb390c12e86
Avishmita Shivkumar <avishmita@gmail.com>
2023-06-23 13:20:28 -0400

    Av-ProductPage

--- client/src/components/productpage.js
+++ client/src/components/productpage.js
@@ -1,11 +1,40 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom"
 import styled from "styled-components"
+import Loader from "./Loader";
+import ItemComponent from "./ItemComponent";
+
 
 const ProductPage = () => {
+  const { product } = useParams();
+  const [ oneItem, setOneItem ] = useState("");
+  const [ company, setCompany ] = useState("");
 
+  useEffect(() => {
+    fetch(`/api/item/${product}`)
+    .then(response => response.json())
+    .then(parsed => {
+      setOneItem(parsed.data)
+    })
+  }, []);
+
+    useEffect(() => {
+    if(oneItem) { 
+      fetch(`/api/company/${oneItem.companyId}`)
+    .then(response => response.json())
+    .then(parsed => {
+      setCompany(parsed.data)
+    })
+    }
+  }, [oneItem])
+  
+  
   return (
-    <Container>
-      <p>test</p>
-    </Container>
+    <>
+    {!oneItem ? <Loader/> :
+      <ItemComponent oneItem={oneItem} company={company}/>
+    }
+    </>
   )
 
 }
