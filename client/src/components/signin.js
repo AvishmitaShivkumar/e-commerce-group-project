3ebe7500e9a0cbfbf13a9de18498ef97d198b04e
Dahyeon Nam <dahyeon.futuredev@gmail.com>
2023-06-27 20:40:07 -0400

    changed styling for sign in/up

--- client/src/components/signin.js
+++ client/src/components/signin.js
@@ -1,153 +1,180 @@
-import { styled } from "styled-components"
-import { Link, useNavigate } from "react-router-dom"
+import { styled } from "styled-components";
+import { Link, useNavigate } from "react-router-dom";
 import { useState, useEffect, useContext } from "react";
 import { UserContext } from "./UserContext";
 
-
 const SignIn = () => {
-
-  const {currentUser, setCurrentUser} = useContext(UserContext);
+  const { currentUser, setCurrentUser } = useContext(UserContext);
 
   const [password, setPassword] = useState("");
-  const [email, setEmail] = useState("")
+  const [email, setEmail] = useState("");
 
-  const [data, setData] = useState({})
+  const [data, setData] = useState({});
 
   const navigate = useNavigate();
 
-  useEffect(()=>{
+  useEffect(() => {
     fetch("/api/users")
-    .then((response)=>response.json())
-    .then((parsed)=>{
-      setData(parsed.data)
-    })
-  },[])
+      .then((response) => response.json())
+      .then((parsed) => {
+        setData(parsed.data);
+      });
+  }, []);
 
-let _id = null
-const handleClick = ((event)=>{
-  event.preventDefault();
+  let _id = null;
+  const handleClick = (event) => {
+    event.preventDefault();
 
-  if(password !== "" && email !== ""){
-  const foundUser = data.find((user)=>{
-    return password === user.password && email === user.email
-  })
-  _id = foundUser._id
-}
-
-fetch(`/api/user/${_id}`)
-  .then((response) => response.json())
-  .then((parsed) => {
-    if(parsed.status === 200){
-      localStorage.setItem("user", JSON.stringify(parsed.data._id))
-      setCurrentUser(parsed.data._id)
-      navigate("/")
+    if (password !== "" && email !== "") {
+      const foundUser = data.find((user) => {
+        return password === user.password && email === user.email;
+      });
+      _id = foundUser._id;
     }
-    })
-  .catch((error) => {
-      window.alert(error)
-  })
-})
 
+    fetch(`/api/user/${_id}`)
+      .then((response) => response.json())
+      .then((parsed) => {
+        if (parsed.status === 200) {
+          localStorage.setItem("user", JSON.stringify(parsed.data._id));
+          setCurrentUser(parsed.data._id);
+          navigate("/");
+        }
+      })
+      .catch((error) => {
+        window.alert(error);
+      });
+  };
 
-useEffect(()=>{
-  if(currentUser) return navigate("/")
-},[currentUser])
+  useEffect(() => {
+    if (currentUser) return navigate("/");
+  }, [currentUser]);
 
-// fetch (post) to push user info into database
+  // fetch (post) to push user info into database
 
-    return (
-        <>
-            <Container>
-                <FormBox>
-                    <Form onSubmit={handleClick}>
-                        <SignInTitle>Sign in to start shopping!</SignInTitle>
-                        <Label>
-                            Email: 
-                            <Input type="text" onChange={(event)=>setEmail(event.target.value)}></Input>
-                        </Label>
-                        <Label>
-                            Password:
-                            <Input type="password" onChange={(event)=>setPassword(event.target.value)}></Input>
-                        </Label>
-                        <SignInButton>Sign In!</SignInButton>
-                    </Form>
-                    <SignUpTitle>Don't have an account?</SignUpTitle>
-                    <SignUpButton to="/signup">Sign Up!</SignUpButton>
-                </FormBox>
-            </Container>
-        </>
-    )
-}
-
+  return (
+    <>
+      <Container>
+        <FormBox>
+          <Form onSubmit={handleClick}>
+            <SignInTitle>Sign in to start shopping!</SignInTitle>
+            <label>
+              <LabelTitle>Email</LabelTitle>
+              <Input
+                type="text"
+                onChange={(event) => setEmail(event.target.value)}
+              ></Input>
+            </label>
+            <label>
+              <LabelTitle>Password</LabelTitle>
+              <Input
+                type="password"
+                onChange={(event) => setPassword(event.target.value)}
+              ></Input>
+            </label>
+            <SignInButton>Sign In!</SignInButton>
+          </Form>
+          <SignUpTitle>Don't have an account?</SignUpTitle>
+          <SignUpButton to="/signup">Sign Up!</SignUpButton>
+        </FormBox>
+      </Container>
+    </>
+  );
+};
 
 export default SignIn;
 
 const Container = styled.section`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-`
+  margin: auto;
+  margin-top: 5rem;
+  margin-bottom: 5rem;
+  padding: 4em 2em;
+  width: 500px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: var(--color-secondary);
+`;
 
 const Form = styled.form`
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
 const FormBox = styled.div`
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
-const Label = styled.label`
-font-size: 2rem;
-text-align: center;
-`
+const LabelTitle = styled.p`
+  font-size: 1.3rem;
+  margin-bottom: 0;
+`;
 
 const SignInTitle = styled.h1`
-font-size: 4rem;
-text-align: center;
-`
+  font-size: 2rem;
+  font-weight: bold;
+  text-align: center;
+  margin-bottom: 25px;
+`;
 
 const Input = styled.input`
-font-size: 2rem;
-margin: 1rem;
-`
+  font-size: 2rem;
+  margin: 1rem;
+  &:focus {
+    outline: 2px solid var(--color-primary);
+  }
+`;
 
 const SignUpTitle = styled.h2`
-font-size: 3rem;
-text-align: center;
-margin: 0;
-`
+  font-size: 1.5rem;
+  text-align: center;
+  margin-top: 2rem;
+  margin-bottom: 1rem;
+`;
 const SignInButton = styled.button`
-color: black;
-font-size: 2rem;
-text-decoration: none;
-margin: 0;
-background-color: red;
-height: 4rem;
-width: 10rem;
-display: flex;
-justify-content: center;
-align-items: center;
-border-radius: 0.5rem;
-`
+  color: white;
+  font-size: 1.5rem;
+  text-decoration: none;
+  margin-top: 1.5rem;
+  background-color: var(--color-accent);
+  height: 2.5rem;
+  width: 10rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 2rem;
+  border: none;
+  transition: ease-in-out 0.2s;
 
+  &:hover {
+    cursor: pointer;
+    transform: translateY(-1px);
+    box-shadow: rgba(0, 0, 0, 0.5) 0 4px 12px;
+  }
+`;
 
 const SignUpButton = styled(Link)`
-color: black;
-font-size: 2rem;
-text-align: center;
-text-decoration: none;
-margin: 0;
-background-color: green;
-height: 3.5rem;
-width: 9rem;
-display: flex;
-justify-content: center;
-align-items: center;
-border-radius: 0.5rem;
-`
+  color: white;
+  font-size: 1.5rem;
+  text-align: center;
+  text-decoration: none;
+  background-color: var(--color-primary);
+  height: 2.5rem;
+  width: 10rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 2rem;
+  transition: ease-in-out 0.2s;
+
+  &:hover {
+    cursor: pointer;
+    transform: translateY(-1px);
+    box-shadow: rgba(0, 0, 0, 0.5) 0 4px 12px;
+  }
+`;
--- client/src/components/signup.js
+++ client/src/components/signup.js
@@ -3,7 +3,6 @@ import { useNavigate } from "react-router-dom";
 import { useState, useEffect, useContext } from "react";
 import { UserContext } from "./UserContext";
 
-
 const SignUp = () => {
   // useState used to keep the values of names, email and password to be used in the fetch POST
   const [firstName, setFirstName] = useState("");
@@ -11,133 +10,157 @@ const SignUp = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const {currentUser} = useContext(UserContext)
+  const { currentUser } = useContext(UserContext);
 
   const navigate = useNavigate();
 
-  useEffect(()=>{
-    if(currentUser) return navigate("/")
-  },[currentUser])
+  useEffect(() => {
+    if (currentUser) return navigate("/");
+  }, [currentUser]);
 
-// fetch (post) to push user info into database
-const handleSubmit = (event) => {
-  event.preventDefault();
-  fetch("/api/signup", {
+  // fetch (post) to push user info into database
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch("/api/signup", {
       method: "POST",
-      body: JSON.stringify({firstName, lastName, email, password}),
+      body: JSON.stringify({ firstName, lastName, email, password }),
       headers: {
-          "Accept": "application/json",
-          "Content-Type": "application/json"
-      }
-  })
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    })
       .then((response) => response.json())
       .then((parsed) => {
-        if(parsed.status===200){
-          localStorage.setItem("user", JSON.stringify(parsed.userId))
-          navigate("/signupconfirmation")
+        if (parsed.status === 200) {
+          localStorage.setItem("user", JSON.stringify(parsed.userId));
+          navigate("/signupconfirmation");
         }
       })
       .catch((error) => {
-          window.alert(error)
-      })
-}
+        window.alert(error);
+      });
+  };
 
+  useEffect(() => {
+    if (currentUser) return navigate("/");
+  }, [currentUser]);
 
-useEffect(()=>{
-  if(currentUser) return navigate("/")
-},[currentUser])
-
-
-    return (
-        <>
-            <Container>
-                <FormBox>
-                    <Form onSubmit={handleSubmit}>
-                        <SignUpTitle>Sign Up Form</SignUpTitle>
-                        <Label>
-                            First Name: 
-                            <Input type="text" id="firstName" onChange={(e) => setFirstName(e.target.value)}></Input>
-                        </Label>
-                        <Label>
-                            Last Name: 
-                            <Input type="text" id="lastName" onChange={(e) => setLastName(e.target.value)}></Input>
-                        </Label>
-                        <Label>
-                            Email: 
-                            <Input type="email" id="email" onChange={(e) => setEmail(e.target.value)}></Input>
-                        </Label>
-                        <Label>
-                            Password:
-                            <Input type="password" id="password" onChange={(e) => setPassword(e.target.value)}></Input>
-                        </Label>
-                        <ButtonDiv>
-                          <SignUpButton>Sign Up!</SignUpButton>
-                        </ButtonDiv>
-                    </Form>
-                </FormBox>
-            </Container>
-        </>
-    )
-}
+  return (
+    <>
+      <Container>
+        <FormBox>
+          <Form onSubmit={handleSubmit}>
+            <SignUpTitle>Sign Up Form</SignUpTitle>
+            <label>
+              <LabelTitle>First Name</LabelTitle>
+              <Input
+                type="text"
+                id="firstName"
+                onChange={(e) => setFirstName(e.target.value)}
+              ></Input>
+            </label>
+            <label>
+              <LabelTitle>Last Name</LabelTitle>
+              <Input
+                type="text"
+                id="lastName"
+                onChange={(e) => setLastName(e.target.value)}
+              ></Input>
+            </label>
+            <label>
+              <LabelTitle>Email</LabelTitle>
+              <Input
+                type="email"
+                id="email"
+                onChange={(e) => setEmail(e.target.value)}
+              ></Input>
+            </label>
+            <label>
+              <LabelTitle>Password</LabelTitle>
+              <Input
+                type="password"
+                id="password"
+                onChange={(e) => setPassword(e.target.value)}
+              ></Input>
+            </label>
+            <ButtonDiv>
+              <SignUpButton>Sign Up!</SignUpButton>
+            </ButtonDiv>
+          </Form>
+        </FormBox>
+      </Container>
+    </>
+  );
+};
 
 export default SignUp;
 
 const Container = styled.section`
-  margin: 3rem;
+  margin: auto;
+  margin-top: 5rem;
+  margin-bottom: 5rem;
+  padding: 4em 2em;
+  width: 500px;
   display: flex;
   justify-content: center;
   align-items: center;
-`
+  background-color: var(--color-secondary);
+`;
 
 const Form = styled.form`
-display: flex;
-flex-direction: column;
-
-`
+  display: flex;
+  flex-direction: column;
+`;
 
 const FormBox = styled.div`
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
-const Label = styled.label`
-font-size: 2rem;
-text-align: right;
-margin: 1rem 0;
-`
+const LabelTitle = styled.p`
+  font-size: 1.3rem;
+  margin-bottom: 0;
+`;
 
 const SignUpTitle = styled.h1`
-font-size: 4rem;
-text-align: center;
-margin: 2rem;
-`
+  font-size: 2rem;
+  font-weight: bold;
+  text-align: center;
+  margin-bottom: 25px;
+`;
 
 const Input = styled.input`
-font-size: 2rem;
-margin: 1rem;
-width: 35rem;
-`
+  font-size: 2rem;
+  margin: 1rem;
+  &:focus {
+    outline: 2px solid var(--color-primary);
+  }
+`;
 
 const SignUpButton = styled.button`
-color:black;
-font-size: 3rem;
-text-align: center;
-text-decoration: none;
-margin: 0;
-width: 15rem;
-height: 7rem;
-border-radius: 2rem;
+  color: white;
+  background-color: var(--color-primary);
+  font-size: 1.5rem;
+  text-align: center;
+  text-decoration: none;
+  width: 10rem;
+  height: 2.5rem;
+  border-radius: 2rem;
+  border: none;
+  margin-top: 1rem;
 
-&:hover{
-  background-color: red;
-}
+  &:hover {
+    background-color: var(--color-accent);
+    cursor: pointer;
+    box-shadow: rgba(0, 0, 0, 0.5) 0 4px 12px;
+    transform: translateY(-1px);
+  }
 
-transition: ease-in-out 0.2s;
-`
+  transition: ease-in-out 0.2s;
+`;
 const ButtonDiv = styled.div`
-margin: 3rem;
-display: flex;
-justify-content: center;
-`
+  display: flex;
+  justify-content: center;
+`;
