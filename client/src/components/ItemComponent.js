21319981fe19631c19484142228b5d4c42c66c15
Avishmita Shivkumar <avishmita@gmail.com>
2023-06-28 11:59:44 -0400

    Waitlist

--- client/src/components/ItemComponent.js
+++ client/src/components/ItemComponent.js
@@ -48,6 +48,10 @@ const ItemComponent = ({oneItem, company}) => {
       })}
   }
 
+  const waitlistHandler = (event) => {
+    event.target.value = "";
+  };
+
     return (
         <ItemContainer>
             <BigImage src={oneItem.imageSrc} />
@@ -61,8 +65,14 @@ const ItemComponent = ({oneItem, company}) => {
         <p>Category: {oneItem.category}</p>
                 <p><StyledLink to={`/company/${oneItem.companyId}`}> Made by: {company.name} </StyledLink></p>
         <Button disabled={oneItem.numInStock === 0} onClick={addToCartClick}>
-        {!oneItem.numInStock > 0 ? "Out of stock" : loading ? "Adding to cart..."  : "Add to cart"}
+        {!oneItem.numInStock > 0 ? "Out of stock"  : loading ? "Adding to cart..."  : "Add to cart"}
         </Button>
+        {!oneItem.numInStock > 0 &&
+         <Waitlist>
+         <WaitlistInput type="email" placeholder="Enter your email"/>
+         <Waitlistbutton onClick={waitlistHandler}>Get on the waitlist</Waitlistbutton>
+          </Waitlist>
+        }
       </InformationContainer>
     </ItemContainer>
   );
@@ -182,3 +192,65 @@ cursor: "pointer";
 }
 
 `
+
+const Waitlist = styled.form`
+display: flex;
+flex-direction: column;
+align-items: center;
+margin: 1.5rem;
+`
+const WaitlistInput = styled.input`
+width: 100%;
+margin-top: 1rem;
+font-size: 1.1rem;
+`
+const Waitlistbutton = styled.button`
+align-items: center;
+  background-color: var(--color-secondary);
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  border-radius: .25rem;
+  box-shadow: rgba(0, 0, 0, 0.02) 0 1px 3px 0;
+  box-sizing: border-box;
+  color: var(--color-accent);
+  cursor: pointer;
+  display: inline-flex;
+  font-family: 'Raleway', sans-serif;
+  font-size: 16px;
+  font-weight: 600;
+  justify-content: center;
+  line-height: 1.25;
+  margin: 0;
+  min-height: 3rem;
+  padding: calc(.875rem - 1px) calc(1.5rem - 1px);
+  position: relative;
+  text-decoration: none;
+  transition: all 250ms;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  vertical-align: baseline;
+  width: auto;
+  margin-top: 2em;
+
+&:hover,
+&:focus {
+  border-color: rgba(0, 0, 0, 0.15);
+  box-shadow: rgba(0, 0, 0, 0.1) 0 4px 12px;
+  color: rgba(217,60,28,0.65);
+}
+
+&:hover {
+  transform: translateY(-1px);
+  
+}
+
+&:active {
+  background-color: var(--color-secondary);
+  border-color: rgba(0, 0, 0, 0.15);
+  box-shadow: rgba(0, 0, 0, 0.06) 0 2px 4px;
+  color: var(--color-accent);
+  transform: translateY(0);
+}
+
+`
+;
